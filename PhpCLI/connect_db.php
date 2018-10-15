7b0c255684b170c99b0252b6a6c30f8b294f3101
Catouillard Benjamin <catouillard.benjamin@gmail.com>
2018-10-15 09:28:39 +0000

    Update connect_db.php

--- PhpCLI/connect_db.php
+++ PhpCLI/connect_db.php
@@ -1,5 +1,11 @@
 <?php
 
+$result = shell_exec("grep 'connect_db.php' /etc/crontab"); // Vérifie si dans le fichier crontab on a le script de connexion à la base de donnée
+
+if(empty($result)){ // On vérifie si le résultat est vide
+    shell_exec("echo '05 * * * * root php /home/cli/connect_db.php"); // Alors on ajoute la connexion à la base de donnée dans le crontab qui sera effectué toutes les 5 minutes.
+}
+
 $conn = new mysqli("localhost", "root", "btsinfo", "Ansible"); //On initialise une variable avec les identifiants afin de permettre la connexion à la base de donnée.
 
 if($conn->connect_error){ // On vérifie si la connexion fonctionne ou non
