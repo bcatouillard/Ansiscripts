619f5ead13f9767449691c8542edbfa060ae9047
Catouillard Benjamin <catouillard.benjamin@gmail.com>
2018-12-03 08:25:13 +0000

    Update connect_db.php

--- PhpCLI/connect_db.php
+++ PhpCLI/connect_db.php
@@ -1,23 +1,29 @@
 <?php
 
-$directory = getcwd()."/";
+$directory = getcwd()."/"; // Obtient le dossier 
 
-$result = shell_exec("crontab -l | grep 'connect_db.php' ");
-if(!empty($grep)){
-	shell_exec("crontab < <(crontab -l; echo '5 * * * * /home/cli/connect_db' ");
+$result = shell_exec("crontab -l | grep 'connect_db.php' "); // On ajoute le résultat dans $result
+if(!empty($grep)){ // Si $grep est vide
+	shell_exec("crontab < <(crontab -l; echo '5 * * * * /home/cli/connect_db' "); // Ajout dans crontab du script à exécuter
+}
+try{
+    $config['serveur']='localhost'; // Adresse du serveur de BDD
+    $config['login'] = 'root'; // Identifiant de connexion à la BDD
+    $config['mdp'] ='btsinfo'; // Mot de passe de la BDD
+    $config['bd'] = 'Ansible'; // Nom de la BDD
+    $conn = new PDO('mysql:host='.$config['serveur'].';dbname='.$config['bd'],$config['login'],$config['mdp']); // Connexion à la BDD
+}
+catch(Exception $e){ // Si on attrape une erreur alors
+  $conn = NULL; // On met la variable de connexion à null
+  echo "\n*****Erreur de Connexion*****\n\n"; // On affiche une erreur
 }
 
-$conn = new mysqli("localhost","root","btsinfo", "Ansible");
+echo "\n*****Connecté*****\n\n"; // Affiche que nous sommes connectés à la base de donnée
 
-if($conn->connect_error){
-	die("ERREUR: Impossible de se connecter : " . $conn->connect_error);
+if($conn != null){ // On vérifie que la connexion est différente de null
+    require_once($directory."getData.php"); // Importe le fichier getData.php
+    getData::addPoste($conn); // Appel à la fonction addPoste du fichier getData.php
+    getData::getTasks($conn); // Appel à la fonction getTasks du fichier getData.php
 }
-
-echo "\n*****Connecté*****\n\n";
-
-require_once($directory."getData.php");
-addPoste($conn);
-getTasks($conn);
-
-$conn->close();
-?>
+$conn->close(); // Fermeture de la connexion à la base de donnée
+?> 
