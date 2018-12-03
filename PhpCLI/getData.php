8999af2030d526d6b00d5fc2ddbc64dd340450f2
Catouillard Benjamin <catouillard.benjamin@gmail.com>
2018-12-03 08:07:04 +0000

    Update getData.php

--- PhpCLI/getData.php
+++ PhpCLI/getData.php
@@ -18,40 +18,39 @@ Class getData {
     }
 
     function xmlConvert($conn, $directory, $filename, $query, $type){
-            	$result =  $conn->query($query);
-            	$data = $result->fetchAll();
+            	$result =  $conn->query($query); // Exécute la requête
+            	$data = $result->fetchAll(); // Récupère tout les résultats et les stocke dans la variable $data
 		
-                $xml = new DOMDocument();
+                $xml = new DOMDocument(); // Création d'un objet DOMDocument
                 $xml->formatOutput = TRUE;
                 
-                if($data != null){
-                    $xml_type = $xml->createElement($type);
-                    if($type == "Postes"){
+                if($data != null){ // On vérifie que le tableau n'est pas nul
+                    $xml_type = $xml->createElement($type); // On crée un Element dans notre fichier XML qui correspond au type de donnée
+                    if($type == "Postes"){ // On vérifie si le type vaut Postes
                         foreach ($data as $key => $value) {
-                            $xml_poste = $xml->createElement("Poste");
-                            $xml_poste->setAttribute( "nom", $value['nom'] );
-                            $xml_ip = $xml->createElement("adresseIP", $value['adresseIP']);
-                            $xml_poste->appendChild($xml_ip);
-                            $xml_type->appendChild($xml_poste);
-                            $xml->appendChild($xml_type);
+                            $xml_poste = $xml->createElement("Poste"); // On crée un Element pour un Poste
+                            $xml_poste->setAttribute( "nom", $value['nom'] ); // On lui attribue le nom du Poste
+                            $xml_ip = $xml->createElement("adresseIP", $value['adresseIP']); // On crée un Element qui sera l'adresse IP
+                            $xml_poste->appendChild($xml_ip); // On ajoute l'adresse IP du poste à $xml_poste 
+                            $xml_type->appendChild($xml_poste); // On ajoute $xml_poste à $xml_type
+                            $xml->appendChild($xml_type); // On ajoute $xml_type au fichier xml
                         }
-                    } else{
+                    } else{ // Si $type ne vaut pas Postes alors
                         foreach ($data as $key => $value) {
-                            var_dump($value);
-                            $xml_cmd = $xml->createElement("Commande");
-                            $xml_cmd->setAttribute( "cmd", $value['cmd'] );
-                            $xml_ip = $xml->createElement("adresseIP", $value['ipPoste']);
-                            $xml_cmd->appendChild($xml_ip);
-                            $xml_type->appendChild($xml_cmd);
-                            $xml->appendChild($xml_type);
+                            $xml_cmd = $xml->createElement("Commande"); // On crée un Element pour une commande
+                            $xml_cmd->setAttribute( "cmd", $value['cmd'] ); // On lui ajoute la commande en attribut
+                            $xml_ip = $xml->createElement("adresseIP", $value['ipPoste']); // On crée l'Element qui contient l'adresse IP
+                            $xml_cmd->appendChild($xml_ip); // On ajoute l'ip à la commande
+                            $xml_type->appendChild($xml_cmd); // On ajoute la commande au type
+                            $xml->appendChild($xml_type); // Enfin on ajoute la commande au fichier XML
                         }
                     }
                     
-                    if(!is_dir($directory)){
-                                shell_exec("mkdir $directory");
+                    if(!is_dir($directory)){ // Si notre dossier n'existe pas alors
+                                shell_exec("mkdir $directory"); // On le crée
                     }
                     
-                    $xml->save($directory.$filename);
+                    $xml->save($directory.$filename); // On enregistre le fichier XML dans le dossier avec son nom
                 }
     }
 }
